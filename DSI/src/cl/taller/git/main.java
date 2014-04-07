8fe3253f77ad29e4338605247fca3152a82656ad
Totaldo <Totaldo@10.3.7.186>
2014-04-07 17:20:37 -0300

    modificado local

--- DSI/src/cl/taller/git/main.java
+++ DSI/src/cl/taller/git/main.java
@@ -4,7 +4,7 @@ public class main {
 
 	public static void main(String[] args) {
 		System.out.println("Mi segundor commit");
-		System.out.println("Linea agregada desde la interflex");
+		System.out.println("Linea agregada desde la interflex modificado");
 
 	}
 
